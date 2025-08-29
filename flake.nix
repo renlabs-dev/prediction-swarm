be75690931cb863bde272ff97d04bb476fa9711b
PsicoThePato <jmomcontato@gmail.com>
2025-08-29 10:07:07 -0300

    nix

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Swarm Evaluator - Honza is very cool";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+          config.allowUnfree = true;
+        };
+        python = pkgs.python312;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            python
+            pkgs.uv
+            pkgs.ruff
+          ];
+        };
+      });
+}
